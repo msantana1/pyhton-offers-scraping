4cf25095ad5cd55dbde93ae94b25fde5170678c6
Miguel Angel <miguel.a91@outlook.com>
2023-03-05 12:39:05 -0600

    sensitive date removed and passed on parameters

--- main.py
+++ main.py
@@ -8,14 +8,12 @@ data = [["https://www.cyberpuerta.mx/Computadoras/Laptops/Laptop-Dell-Inspiron-5
         "https://www.cyberpuerta.mx/Computadoras/Laptops/Laptop-HP-EliteBook-845-G9-14-WUXGA-AMD-Ryzen-7-PRO-6850HS-3-20GHz-16GB-512GB-SSD-Windows-10-Pro-64-bit-Espanol-Plata.html",
         "https://www.cyberpuerta.mx/Computadoras/Laptops/Laptop-ASUS-Vivobook-S-D413UA-14-Full-HD-AMD-Ryzen-7-5700U-1-80GHz-16GB-512GB-SSD-Windows-10-Home-64-bit-Espanol-Oro-Rosado.html",
         "https://www.cyberpuerta.mx/Computadoras/Laptops/Laptop-Lenovo-IdeaPad-3-15ALC6-15-6-Full-HD-AMD-Ryzen-7-5700U-1-80GHz-16GB-512GB-SSD-Windows-11-Home-64-bit-Espanol-Arena.html"],
-        ["16529", "15819", "16559", "24179", "17699", "19319"]]
+        ["16529", "15819", "16559", "24179", "17699", "19309"]]
 
-def send_mail(name, url1, price, password):
+def send_mail(name, url1, price, password, sender_email, receiver_email):
     new_name = name.replace("ñ", "n")
     port = 587  # For starttls
     smtp_server = "smtp.office365.com"
-    sender_email = "miguel-ofertas@outlook.com"
-    receiver_email = "miguel.a91@outlook.com"
     message ="""\
     Subject: OFERTA %s
 
@@ -25,6 +23,7 @@ def send_mail(name, url1, price, password):
     Precio: %s""" % (new_name, new_name, url1, price)
 
     print(message)
+    #print(password)
 
     context = ssl.create_default_context()
     with smtplib.SMTP(smtp_server, port) as server:
@@ -41,7 +40,9 @@ if "__main__" == __name__:
     prices = data[1]
 
     password = sys.argv[1]
-    
+    sender_email = sys.argv[2]
+    receiver_email = sys.argv[3] 
+
     count = 0
     for url in urls:
         page = requests.get(url)
@@ -55,4 +56,4 @@ if "__main__" == __name__:
         current_price = prices[count]
         count += 1
         if int(price) < int(current_price) and availability == "InStock":
-            send_mail(name, url1, price, password)
+            send_mail(name, url1, price, password, sender_email, receiver_email)
