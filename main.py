1839cfec43f44090c31965569b9e7ff45d5ddffc
Miguel Angel <miguel.a91@outlook.com>
2023-03-08 23:38:01 -0600

    script py working

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ data = [["https://www.cyberpuerta.mx/Computadoras/Laptops/Laptop-Dell-Inspiron-5
         "https://www.cyberpuerta.mx/Computadoras/Laptops/Laptop-ASUS-Vivobook-S-D413UA-14-Full-HD-AMD-Ryzen-7-5700U-1-80GHz-16GB-512GB-SSD-Windows-10-Home-64-bit-Ingles-Negro.html",
         "https://www.cyberpuerta.mx/Computadoras/Laptops/Laptop-HP-EliteBook-845-G9-14-WUXGA-AMD-Ryzen-7-PRO-6850HS-3-20GHz-16GB-512GB-SSD-Windows-10-Pro-64-bit-Espanol-Plata.html",
         "https://www.cyberpuerta.mx/Computadoras/Laptops/Laptop-ASUS-Vivobook-S-D413UA-14-Full-HD-AMD-Ryzen-7-5700U-1-80GHz-16GB-512GB-SSD-Windows-10-Home-64-bit-Espanol-Oro-Rosado.html",
-        "https://www.cyberpuerta.mx/Computadoras/Laptops/Laptop-Lenovo-IdeaPad-3-15ALC6-15-6-Full-HD-AMD-Ryzen-7-5700U-1-80GHz-16GB-512GB-SSD-Windows-11-Home-64-bit-Espanol-Arena.html"],
-        ["16529", "15819", "16559", "24179", "17699", "19309"]]
+        "https://www.cyberpuerta.mx/Computadoras/Laptops/Laptop-Lenovo-IdeaPad-3-15ALC6-15-6-Full-HD-AMD-Ryzen-7-5700U-1-80GHz-16GB-512GB-SSD-Windows-11-Home-64-bit-Espanol-Arena.html",
+        "https://www.cyberpuerta.mx/Computadoras/Laptops/Laptop-Lenovo-V14-G2-ALC-14-HD-AMD-Ryzen-7-5700U-1-80GHz-16GB-512GB-SSD-Windows-10-Pro-64-bit-Espanol-Gris.html"],
+        ["16529", "15819", "16559", "24179", "17699", "19309", "19245"]]
 
 def send_mail(name, url1, price, password, sender_email, receiver_email):
     new_name = name.replace("ñ", "n")
