ad41c35548e602facd4fb07d6a0d9376bd46cf11
Miguel Angel <miguel.a91@outlook.com>
2023-03-09 23:16:19 -0600

    typos and format errors fixed

--- main.py
+++ main.py
@@ -45,13 +45,13 @@ def urlsCyberpuerta(urls):
 
 if "__main__" == __name__:
 
-    urlsRyzen7 = {    'https://www.cyberpuerta.mx/Computadoras/Laptops/Laptop-Dell-Inspiron-5415-14-Full-HD-AMD-Ryzen-7-5700U-1-80GHz-8GB-512GB-SSD-Windows-10-Home-64-bit-Espanol-Platino.html' : '16529',
-                'https://www.cyberpuerta.mx/Computadoras/Laptops/Laptop-ASUS-Vivobook-S-D413UA-14-Full-HD-AMD-Ryzen-7-5700U-1-80GHz-8GB-512GB-SSD-Windows-11-Home-64-bit-Ingles-Negro.html' : '15819',
-                'https://www.cyberpuerta.mx/Computadoras/Laptops/Laptop-ASUS-Vivobook-S-D413UA-14-Full-HD-AMD-Ryzen-7-5700U-1-80GHz-16GB-512GB-SSD-Windows-10-Home-64-bit-Ingles-Negro.html' : '16559',
-                'https://www.cyberpuerta.mx/Computadoras/Laptops/Laptop-HP-EliteBook-845-G9-14-WUXGA-AMD-Ryzen-7-PRO-6850HS-3-20GHz-16GB-512GB-SSD-Windows-10-Pro-64-bit-Espanol-Plata.html' : '24179',
-                'https://www.cyberpuerta.mx/Computadoras/Laptops/Laptop-ASUS-Vivobook-S-D413UA-14-Full-HD-AMD-Ryzen-7-5700U-1-80GHz-16GB-512GB-SSD-Windows-10-Home-64-bit-Espanol-Oro-Rosado.html' : '17699',
-                'https://www.cyberpuerta.mx/Computadoras/Laptops/Laptop-Lenovo-IdeaPad-3-15ALC6-15-6-Full-HD-AMD-Ryzen-7-5700U-1-80GHz-16GB-512GB-SSD-Windows-11-Home-64-bit-Espanol-Arena.html' : '19309',
-                'https://www.cyberpuerta.mx/Computadoras/Laptops/Laptop-Lenovo-V14-G2-ALC-14-HD-AMD-Ryzen-7-5700U-1-80GHz-16GB-512GB-SSD-Windows-10-Pro-64-bit-Espanol-Gris.html' : '19245' }
+    urlsRyzen7 = {  'https://www.cyberpuerta.mx/Computadoras/Laptops/Laptop-Dell-Inspiron-5415-14-Full-HD-AMD-Ryzen-7-5700U-1-80GHz-8GB-512GB-SSD-Windows-10-Home-64-bit-Espanol-Platino.html' : '16529',
+                    'https://www.cyberpuerta.mx/Computadoras/Laptops/Laptop-ASUS-Vivobook-S-D413UA-14-Full-HD-AMD-Ryzen-7-5700U-1-80GHz-8GB-512GB-SSD-Windows-11-Home-64-bit-Ingles-Negro.html' : '15819',
+                    'https://www.cyberpuerta.mx/Computadoras/Laptops/Laptop-ASUS-Vivobook-S-D413UA-14-Full-HD-AMD-Ryzen-7-5700U-1-80GHz-16GB-512GB-SSD-Windows-10-Home-64-bit-Ingles-Negro.html' : '16559',
+                    'https://www.cyberpuerta.mx/Computadoras/Laptops/Laptop-HP-EliteBook-845-G9-14-WUXGA-AMD-Ryzen-7-PRO-6850HS-3-20GHz-16GB-512GB-SSD-Windows-10-Pro-64-bit-Espanol-Plata.html' : '24179',
+                    'https://www.cyberpuerta.mx/Computadoras/Laptops/Laptop-ASUS-Vivobook-S-D413UA-14-Full-HD-AMD-Ryzen-7-5700U-1-80GHz-16GB-512GB-SSD-Windows-10-Home-64-bit-Espanol-Oro-Rosado.html' : '17699',
+                    'https://www.cyberpuerta.mx/Computadoras/Laptops/Laptop-Lenovo-IdeaPad-3-15ALC6-15-6-Full-HD-AMD-Ryzen-7-5700U-1-80GHz-16GB-512GB-SSD-Windows-11-Home-64-bit-Espanol-Arena.html' : '19309',
+                    'https://www.cyberpuerta.mx/Computadoras/Laptops/Laptop-Lenovo-V14-G2-ALC-14-HD-AMD-Ryzen-7-5700U-1-80GHz-16GB-512GB-SSD-Windows-10-Pro-64-bit-Espanol-Gris.html' : '19245'}
     
     urlsi7 = {  'https://www.cyberpuerta.mx/Computadoras/Laptops/Laptop-Gamer-XPG-Xenia-14-14-Full-HD-Intel-Core-i7-1165G7-2-80GHz-16GB-512GB-SSD-Windows-10-Home-64-bit-Espanol-Negro.html' : '16049',
                 'https://www.cyberpuerta.mx/Computadoras/Laptops/Laptop-Gamer-Dell-Vostro-5310-13-3-Quad-HD-Intel-Core-i7-11390H-3-40GHz-16GB-512GB-SSD-NVIDIA-GeForce-MX450-Windows-11-Pro-64-bit-Espanol-Verde.html' : '22269', 
